c393b2df2b742d9e5d8dc672d4372a053cb95916
nayronferreira <nayronferreira30@gmail.com>
2020-01-26 05:16:58 -0300

    Criando Classe Departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento dep = new Departamento(1, "Nayron");
+		System.out.println(dep);
+	}
+
+}
